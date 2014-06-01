3e9309aebd8e49d1dc4428fa35b7ab169e930f52
Ing Juan Pablo Perez <jperez@global-tronic.com>
2014-05-31 21:10:26 -0500

    Muestra de conexión DB php msql

--- code.php
+++ code.php
@@ -6,7 +6,18 @@ if($_GET['vs']!="4321"){
 header ("Location: http://dev.nemesistracker.com/login.aspx");
 }
 
-include ("inc/connect.php");
+
+$serverName = "dev.nemesistracker.com"; //serverName\instanceName
+$connectionInfo = array( "Database"=>"GPSDB", "UID"=>"sa", "PWD"=>"gps@2013");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+
+
 
 $serial=$_GET['serial'];
 
